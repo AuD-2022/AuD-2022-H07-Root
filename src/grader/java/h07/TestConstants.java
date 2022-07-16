e99cdfb81d02b3540536c0d42a048d22f40d742c
LukasKlenner <klenner-lukas@t-online.de>
2022-07-17 01:29:32 +0200

    adjust constants

--- src/grader/java/h07/TestConstants.java
+++ src/grader/java/h07/TestConstants.java
@@ -7,7 +7,7 @@ public class TestConstants {
     //general
     public static final long SEED = 0L;
     public static final Random RANDOM = new Random(SEED);
-    public static final int ARGUMENT_COUNT = 5;
+    public static final int ARGUMENT_COUNT = 2;
     public static final int TEST_ITERATIONS = 5;
 
     //graphProvider
@@ -21,17 +21,17 @@ public class TestConstants {
     public static final int MAX_PATH_LENGTH = 50;
 
     //Points
-    public static final int POINTS_COUNT_COLLECTION_TEST = 25;
+    public static final int POINTS_COUNT_COLLECTION_TEST = 20;
     public static final int POINTS_COUNT = 5;
     public static final int MAX_ARC_LENGTH_POINT = 10;
     public static final int MIN_CORD = -10;
     public static final int MAX_CORD = 10;
 
     //HeapProvider
-    public static final int HEAP_CAPACITY = 50;
+    public static final int HEAP_CAPACITY = 20;
 
     //ListProvider
-    public static final int LIST_SIZE = 50;
+    public static final int LIST_SIZE = 20;
 
     //QueueEntry
     public static final int MAX_QUEUE_ENTRY_VALUE = 100;
